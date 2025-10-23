9c61c9db407bc57942003d3604beebe1a10a9268
Yuki Yamada <yuki.yamada@kokorozashi-japan.co.jp>
2025-10-23 17:43:08 +0900

    Vitest 用に JSX 設定を整備

--- score-service/app/components/ConfigurationForm.tsx
+++ score-service/app/components/ConfigurationForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
 import {
   FIELD_DEFINITIONS,
